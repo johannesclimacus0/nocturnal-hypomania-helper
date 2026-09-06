cc34e7ee805d59d25277b53b79502af2b6b5ec58
johannesclimacus0 <dhdhfgffifid@gmail.com>
2026-09-06 18:20:33 +1000

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main class="min-h-screen flex items-center justify-center bg-zinc-950 text-white">
+        <p>hi</p>
+    </main>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.ts',
+    ])
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
